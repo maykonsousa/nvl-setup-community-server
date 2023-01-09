d0c3bc341a16d2d73cd284a0f2c8ceb2b69876e0
Maykon Eduardo Pereira de Sousa <maeduardo@uolinc.com>
2023-01-09 17:54:12 -0300

    fix: outra tentativa de ajuste de data

--- src/helpers/FormatDate.ts
+++ src/helpers/FormatDate.ts
@@ -2,7 +2,7 @@ import { format, parseISO } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export default function formatDate(date: Date): string {
-  return format(parseISO(date.toISOString()), "dd/MM/yyyy HH:mm:ss", {
+  return format(date, "dd/MM/yyyy HH:mm:ss", {
     locale: ptBR,
   });
 }
