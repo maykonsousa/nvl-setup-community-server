c3c0ac7fd6f84d2953e5fc65d9a0605b6dc65372
Maykon Eduardo Pereira de Sousa <maeduardo@uolinc.com>
2023-01-13 09:27:34 -0300

    tentativa de ajustar os horarios

--- src/helpers/FormatDate.ts
+++ src/helpers/FormatDate.ts
@@ -2,6 +2,8 @@ import { format, parseISO } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export default function formatDate(date: Date): string {
+  //remove 3 hours from date
+  date.setHours(date.getHours() - 3);
   return format(date, "dd/MM/yyyy HH:mm:ss", {
     locale: ptBR,
   });
